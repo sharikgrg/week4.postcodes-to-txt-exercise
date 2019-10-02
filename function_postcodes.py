9c06356c26e7ebee75f08ccb3dafed08b68d42f0
Sharik Gurung <SGurung@spartaglobal.com>
2019-10-02 17:29:51 +0100

    append function created

--- function_postcodes.py
+++ function_postcodes.py
@@ -2,4 +2,14 @@ import requests
 def importing_functions(postcode):
     request_postcode = requests.get('http://api.postcodes.io/postcodes/' + postcode)
     retrieving_data = request_postcode.json()
-    return retrieving_data
+    postcode = retrieving_data['result']['postcode']
+    longitude = retrieving_data['result']['longitude']
+    latitude = retrieving_data['result']['latitude']
+    print(postcode, longitude, latitude)
+
+def append_to_file(file, postcode):
+    try:
+        with open(file,'a') as opened_file:
+            opened_file.write(postcode + '\n')
+    except FileNotFoundError:
+        print('File not found')
--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 from function_postcodes import *
+list = []
+postcode1 = importing_functions('TN234QY')
+postcode2 = importing_functions('E162RJ')
+postcode3 = importing_functions('E162RD')
+list.append(postcode1)
+list.append(postcode2)
+list.append(postcode3)
+for datas in list:
+    append_to_file('postcode_dets.txt', datas)
